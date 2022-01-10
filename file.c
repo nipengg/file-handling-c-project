385dccf7cae6f1f5113a148c83ae311917069533
nipengg <nevillecorneliustj@gmail.com>
2022-01-10 21:40:16 +0700

    fix

--- file.c
+++ file.c
@@ -1,85 +1,194 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
+typedef struct tanggal{
+		int hari;
+		int bulan;
+		int tahun;
+}tanggal;
+
 typedef struct ticket{
-    char nameCustomer[20];
-    char eventType[20];
-    char eventName[20];
-    int ticketTotal;
-    int ticketPrice;
-    int total;
+	char customerName[10];
+	char eventType[20];
+	char eventName[20];
+	int ticketTotal;
+	int ticketPrice;
+	int total;
+	tanggal tanggal;
 }ticket;
+ticket data[100];
 
-void create()
+int count;
+void readData()
 {
-    ticket *t;
-    FILE *fp;
-    int n, i, j;
-    printf("Enter how many tickets you want to create? : ");
-    scanf("%d", &n);
-
-    t = (ticket*)calloc(n, sizeof(ticket));
-    fp = fopen("sales.txt", "a");
-
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("Ticket #%d : \n", i + 1);
-        t[i].total = 0;
-        fflush(stdin);
-        printf("Enter Customer Name : ");
-        scanf("%[^\n]s", t[i].nameCustomer);
-        fflush(stdin);
-        printf("Enter Event Type    : ");
-        scanf("%[^\n]s", t[i].eventType);
-        fflush(stdin);
-        printf("Enter Event Name    : ");
-        scanf("%[^\n]s", t[i].eventName);
-        fflush(stdin);
-        printf("Enter Total Ticket  : ");
-        scanf("%d", &t[i].ticketTotal);
-        printf("Enter Ticket Price  : ");
-        scanf("%d", &t[i].ticketPrice);
-        printf("\n");
-        t[i].total  = t[i].ticketTotal * t[i].ticketPrice;
-        fwrite(&t[i], sizeof(ticket), 1, fp);
-    }
-    fclose(fp);
+	FILE *fp = fopen("sales.txt", "r");
+	count = 0;
+	char customerName[10], eventType[100], eventName[100];
+	int ticketTotal, ticketPrice, total, hari, bulan, tahun;
+	while (fscanf(fp, "%s %s %s %d %d %d %d/%d/%d\n", customerName, eventType, eventName, &ticketTotal, &ticketPrice, &total, &hari, &bulan, &tahun) != EOF)
+	{
+		strcpy(data[count].customerName, customerName);
+		strcpy(data[count].eventType, eventType);
+		strcpy(data[count].eventName, eventName);
+		data[count].ticketTotal, ticketTotal;
+		data[count].ticketPrice, ticketPrice;
+		data[count].total, total;
+		data[count].tanggal.hari = hari;
+		data[count].tanggal.bulan = bulan;
+		data[count].tanggal.tahun = tahun;
+		count++;
+	}
+	fclose(fp);
 }
 
 void display()
 {
-    ticket t1;
-    FILE *fp;
-    fp = fopen("sales.txt", "r");
-    if (fp == NULL)
+	if (count == 0)
     {
-        printf("[ERROR] File does not exist. Type 1 to create file\n");
-        system("pause");
+		printf("[ERROR] File does not exist. Type 1 to create file\n");
+		system("pause");
+	}
+    else
+    {
+		FILE * fp = fopen("sales.txt", "r");
+		char customerName[100], eventType[100], eventName[100], date[100];
+		int ticketTotal, ticketPrice, total, i = 0;
+		printf("No. | Transaction Date  |  Customer Name       |  Event Type       | Event Name		 | Total Ticket |  Ticket Price    |      Total       |\n");
+  		printf("=================================================================================================================================================\n");
+		while (fscanf(fp, "%s %s %s %s %d %d %d\n", date, customerName, eventType, eventName, &ticketTotal, &ticketPrice, &total) != EOF)
+        {
+			printf("%-3d | %-17s | %-20s | %-17s | %-19s | %-12d | Rp.%-13d | Rp.%-13d |\n", ++i , date, customerName, eventType, eventName, ticketTotal, ticketPrice, total);
+		}
+		printf("=================================================================================================================================================\n");
+		system("pause");
+	}
+}
+
+int checkDate(char date[])
+{
+	if ((strlen(date) < 8 || strlen(date) > 8) && (date[2] != '/' || date[5] != '/'))
+    {
+        return 0;
     }
     else
     {
-        int j, i = 0;
-        printf("No. |  Nama           |  Event Type     |  Event Name    |  Ticket Total   |  Ticket Price   |  Total Price    |\n");
-  		printf("================================================================================================================\n");
-        while (fread(&t1, sizeof(ticket), 1, fp))
-        {
-            printf("%-3d | %-15s | %-15s | %-14s | %-15d | Rp.%-12d | Rp.%-12d |\n", ++i, t1.nameCustomer, t1.eventType, t1.eventName, t1.ticketTotal, t1.ticketPrice, t1.total);
-        }
-        printf("================================================================================================================\n");
-        fclose(fp);
-        system("pause");
+        return 1;
     }
+}
+
+void create()
+{
+	char date[100];
+	int n;
+	printf("Enter how many tickets you want to create? : ");
+	scanf("%d", &n);
+	FILE *fp = fopen("sales.txt" , "a");
+	for (int i = 0; i < n; i++)
+	{
+		printf("Ticket #%d : \n", i + 1);
+		fflush(stdin);
+		do
+		{
+			printf("Enter Transaction Date [dd/mm/yy]: ");
+			scanf("%s", date);
+		}while(checkDate(date) == 0);
+
+		char customerName[100];
+		fflush(stdin);
+		printf("Enter Customer Name: ");
+		scanf("%[^\n]s", customerName);
+		char eventType[100];
+		fflush(stdin);
+		printf("Enter Event Type   : ");
+		scanf("%s", eventType);
+		char eventName[100];
+		fflush(stdin);
+		printf("Enter Event Name   : ");
+		scanf("%s", eventName);
+		int ticketTotal;
+		fflush(stdin);
+		printf("Enter Total Ticket : ");
+		scanf("%d", &ticketTotal);
+		int ticketPrice;
+		fflush(stdin);
+		printf("Enter Ticket Price : ");
+		scanf("%d", &ticketPrice);
+		int total;
+		total  = ticketTotal * ticketPrice;
+
+		fprintf(fp, "\n%s %s %s %s %d %d %d\n", date, customerName, eventType, eventName, ticketTotal, ticketPrice, total);
+	}
+	fclose(fp);
+	system("pause");
+}
+
+void sortByDate()
+{
+	struct tanggal temp;
+	int i = 0, T, j;
+	char str;
+
+	FILE *fp = fopen("sales.txt", "r");
+	if (fp == NULL)
+	{
+		printf("\n Belum ada data \n");
+		system("pause");
+	}
+
+	while (str != EOF)
+	{
+		fscanf(fp, "%d/%d/%d %s %s %s %d %d %d\n", &data[i].tanggal.hari, &data[i].tanggal.bulan, &data[i].tanggal.tahun, data[i].customerName, data[i].eventType, data[i].eventName, &data[i].ticketTotal, &data[i].ticketPrice, &data[i].total);
+		str = fgetc(fp);
+		i++;
+	}
+
+	T = i - 1;
+
+	// DATE SORT
+	for (i = 1; i <= T; i++)
+    {
+		for (j = 0; j <= T - i; j++)
+        {
+		    if (data[j + 1].tanggal.tahun < data[j].tanggal.tahun)
+		    {
+			    ticket temp = data[j];
+			    data[j] = data[j + 1];
+			    data[j + 1] = temp;
+		    }
+		    if (data[j + 1].tanggal.tahun == data[j].tanggal.tahun && data[j + 1].tanggal.bulan < data[j].tanggal.bulan)
+		    {
+			    ticket temp = data[j];
+			    data[j] = data[j + 1];
+			    data[j + 1] = temp;
+		    }
+		    if (data[j + 1].tanggal.tahun == data[j].tanggal.tahun && data[j + 1].tanggal.bulan == data[j].tanggal.bulan && data[j + 1].tanggal.hari < data[j].tanggal.hari)
+		    {
+			    ticket temp = data[j];
+			    data[j] = data[j + 1];
+			    data[j + 1] = temp;
+		    }
+		}
+	}
+
+	fp = fopen("sales.txt","w");
+	for (i = 0; i <= T; i++)
+    {
+        fprintf(fp,"\n%02d/%02d/%02d %s %s %s %d %d %d\n", data[i].tanggal.hari, data[i].tanggal.bulan, data[i].tanggal.tahun, data[i].customerName, data[i].eventType, data[i].eventName, data[i].ticketTotal, data[i].ticketPrice, data[i].total);
+    }
+	fclose(fp);
 }
 
 int main()
 {
-    int menu;
-    do
+	int menu;
+	readData();
+	do
     {
         printf("\n1. Create\n");
         printf("2. Display\n");
-        printf("3. Exit\n");;
+        printf("3. Sort by Date\n");
+        printf("4. Exit\n");;
 
         printf("Enter Menu : ");
         scanf("%d", &menu);
@@ -93,7 +202,11 @@ int main()
         case 2:
             display();
             break;
-        }
 
-    } while (menu != 3);
+        case 3:
+            sortByDate();
+            display();
+            break;
+        }
+    } while (menu != 4);
 }
